19b62cceaf58b950e9b2b347be151d11e7943371
Emilio A. Escobar <emilio@flumeirc.io>
2026-04-01 17:32:17 -0700

    Move formula to Formula/ directory (Homebrew convention)

---
+++ Formula/flume.rb
@@ -0,0 +1,19 @@
+class Flume < Formula
+  desc "Modern terminal IRC client with scripting and LLM support"
+  homepage "https://github.com/FlumeIRC/flume"
+  url "https://github.com/FlumeIRC/flume/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "PLACEHOLDER_SHA256"
+  license "Apache-2.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "flume-tui")
+    bin.install "target/release/flume"
+    man1.install "doc/flume.1"
+  end
+
+  test do
+    assert_match "Flume", shell_output("#{bin}/flume --version 2>&1", 1)
+  end
+end
